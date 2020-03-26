d8386aa82989ccf476fb07fecb1cd19ae1174f05
abrarhasinkml <ahasin12@gmail.com>
2020-03-26 21:28:25 +0600

    Initial Commit with Scrapping and Plotting completed

---
+++ iedcr_scraper.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 02:51:06 2020
+
+@author: Acer
+"""
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import timer
+import pandas as pd
+
+##LETS GET THE PAGE FIRST
+iedcr_url="https://www.iedcr.gov.bd"
+webpg=requests.get(iedcr_url).text
+##MAKING THE SOUP
+bs4soup=BeautifulSoup(webpg, "html.parser")
+#LETS SCRAPE OUT THE DATE FIRST
+dash=bs4soup.find('div', class_='covid-dash-div')
+findingdate=dash.findAll('p')
+datetext=findingdate[-2].text
+#GOT THE DATE, NOW TO FILTER OUT THE TEXT
+currentdate=''.join([c for c in datetext if c in '0123456789-'])
+#LETS GET THE NUMBERS NOW
+info_table=bs4soup.findAll('div', class_='counter')
+daily_data=[]
+daily_data.append(currentdate)
+for i in info_table:
+    daily_data.append(i.text)
+print(daily_data)
+
+#LETS DISCOVER SOME MORE VALUES FROM THE INFORMATION WE HAVE
+recovery_rate=(int(daily_data[5])/int(daily_data[3]))*100
+death_rate=(int(daily_data[6])/int(daily_data[3]))*100
+daily_data.append(recovery_rate)
+daily_data.append(death_rate)
+
+getPrevData=pd.read_csv('/covid_bd/data.csv')
+recoveredTotal=getPrevData['Recovered']
+#Let's also record the number of daily recoveries
+recoveredToday=int(daily_data[5])-int(recoveredTotal.iloc[-1])
+#Let's append this information to our daily_data
+daily_data.append(recoveredToday)
+
+#Now let's get the daily death toll
+deathTotal=getPrevData['Death Cases']
+deathToday=int(daily_data[6])-int(deathTotal.iloc[-1])
+daily_data.append(deathToday)
+
+##TOTAL COVID-19 TESTS, LAST 24 Hours Test, Covid-19 Positive Cases, Last 24Hours Cases, Recovered, Death Cases##
+cols=['Date','TOTAL COVID-19 TESTS', 'LAST 24 Hours Test', 'Covid-19 Positive Cases', 'Last 24Hours Cases', 'Recovered', 'Death Cases', 'Recovery Rate', 'Death Rate', 'New Recoveries', 'Deaths in last 24 hours']
+df=pd.DataFrame([daily_data],columns=cols)
+
+df.to_csv('/covid_bd/data.csv', mode='a', header=False, index=False)    
---
+++ plotfromData.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 13:47:16 2020
+
+@author: Acer
+"""
+
+import pandas as pd
+
+import plotly.express as px
+from plotly.offline import plot
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+data=pd.read_csv('/covid_bd/data.csv')
+
+data.head()
+
+
+#LETS START PLOTTING
+
+#LETS LOOK AT A BAR PLOT FIRST OF THE TOTAL NUMBER OF POSITIVE COVID PATIENTS PER DAY
+bar=px.bar(data, x='Date', y='Covid-19 Positive Cases')
+bar.update_layout(title_text="TOTAL NUMBER OF CASES AS OF TODAY")
+###IF YOU ARE USING SPYDER IDE THEN USE THIS
+#plot(bar)
+###OTHERWISE JUST GO AHEAD WITH THIS
+#bar.show()
+
+#LETS ALSO PLOT A BAR CHART FOR THE NUMBER OF CASES PER DAY
+dailybar=px.bar(data, x="Date", y='Last 24Hours Cases')
+dailybar.update_layout(title_text="DAILY NEW CASES")
+plot(dailybar)
+
+#NOW LETS TAKE A LOOK AT THE NUMBER OF DAILY RECOVERIES AND DEATHS
+lineplot=make_subplots(specs=[[{'secondary_y':True}]])
+lineplot.add_trace(go.Scatter(x=data["Date"], y=data['Recovered'], name='Recoveries', line=dict(color='green', width=4)), secondary_y=False)
+lineplot.add_trace(go.Scatter(x=data["Date"], y=data['Death Cases'], name='Deaths',line=dict(color='red', width=4)), secondary_y=True)
+lineplot.update_layout(title_text="Daily Recoveries and Death Cases in Bangladesh")
+lineplot.update_xaxes(title_text="Date")
+lineplot.update_yaxes(title_text="<b>Recoveries</b>", secondary_y=False)
+lineplot.update_yaxes(title_text="<b>Deaths</b>", secondary_y=True)
+#plot(lineplot)
+#lineplot.show()
+
+
+#PLOTTING A LINE PLOT FOR THE TOTAL DEATHS
+totDeaths=px.line(data, x='Date', y='Death Cases', text=data['Death Cases'])
+totDeaths.update_layout(title_text="TOTAL NUMBER OF DEATHS")
+#plot(totDeaths)
+#totDeaths.show()
+
+#PLOTTING A BARCHART SHOWING THE DAILY DEATHS
+dailyDeaths=px.bar(data, x='Date', y='Deaths in last 24 hours')
+dailyDeaths.update_layout(title_text="TOTAL NUMBER OF DEATHS ON A DAILY BASIS")
+#plot(dailyDeaths)
+#dailyDeaths.show()
+
+#PLOTTING A LINE TO SHOW THE TOTAL NUMBER OF RECOVERIES 
+totRec=px.line(data, x='Date', y='Recovered', text=data['Recovered'])
+totRec.update_layout(title_text="TOTAL NUMBER OF RECOVERIES")
+#plot(totRec)
+#totRec.show()
+
+
+#PLOTTING A BARCHART SHOWING THE DAILY RECOVERIES
+dailyRec=px.bar(data, x='Date', y='New Recoveries')
+dailyRec.update_layout(title_text="TOTAL NUMBER OF Recoveries ON A DAILY BASIS")
+#plot(dailyRec)
+#dailyRec.show()
+
+#PLOTTING THE RECOVERY RATE AGAINST THE DEATH RATE IN BANGLADESH
+lineplot2=make_subplots(specs=[[{'secondary_y':True}]])
+lineplot2.add_trace(go.Scatter(x=data["Date"], y=data['Recovery Rate'], name='Recovery Rate', line=dict(color='green', width=4)), secondary_y=False)
+lineplot2.add_trace(go.Scatter(x=data["Date"], y=data['Death Rate'], name='Death Rate',line=dict(color='red', width=4)), secondary_y=True)
+lineplot2.update_layout(title_text="Recovery Rate vs Death Rate in Bangladesh")
+lineplot2.update_xaxes(title_text="Date")
+lineplot2.update_yaxes(title_text="<b>Recovery Rate</b>", secondary_y=False)
+lineplot2.update_yaxes(title_text="<b>Death Rate</b>", secondary_y=True)
+plot(lineplot2)
+#lineplot2.show()
