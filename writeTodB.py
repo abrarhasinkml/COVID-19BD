0660107f8b70246316b6787651e0bd922f45b81a
abrarhasinkml <ahasin12@gmail.com>
2020-04-05 03:42:31 +0600

    Added a new script that writes data to MySQL database, everything apart from date being stored perfectly

---
+++ writeTodB.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 00:41:36 2020
+
+@author: Acer
+"""
+
+import pandas as pd
+import numpy as np
+import mysql.connector
+from mysql.connector import Error
+import datetime
+#Read the csv file into a dataframe first
+iedcr_data=pd.read_csv('data.csv')
+#get the last update
+get_last_update=iedcr_data.iloc[-1]
+print(get_last_update)
+
+def DftoSql(dataframe):
+    h=dataframe.columns
+    
+    try:
+        conn=mysql.connector.connect(host='localhost', user='root', password='', database='covid19bd', charset='utf8')
+        #Creating cursor
+        cur=conn.cursor()
+        cur.execute('delete from iedcrdata')
+        conn.commit()
+        for(row, rs) in dataframe.iterrows():
+            temp=rs[0]
+            a=datetime.datetime.strptime(temp, "%d/%m/%Y").date()
+            print(type(a))
+            print(a)
+            b=rs[1]
+            c=rs[2]
+            d=rs[3]
+            e=rs[4]
+            f=rs[5]
+            g=rs[6]
+            h=rs[7]
+            i=rs[8]
+            j=rs[9]
+            k=rs[10]
+            query="insert into iedcrdata values ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})".format(a,b,c,d,e,f,g,h,i,j,k) 
+            #query="insert into iedcrdata values ("+ a +",'"+ b +"','"+ c +"','"+ d +"','"+ e +"','"+ f +"','"+ g +"','"+ h +"','"+ i +"','"+ j +"','"+ k +"')" 
+            cur.execute(query)
+        conn.commit()
+        cur.close()
+    except Error as e:
+        print("Error in MySQL connection :", e)
+    finally:
+        conn.close()
+        
+
+DftoSql(iedcr_data)
+
+            
+
+
