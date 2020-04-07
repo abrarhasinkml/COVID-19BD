23558a50c90d1c80083068747128d643a5ee337c
abrarhasinkml <ahasin12@gmail.com>
2020-04-07 16:18:04 +0600

    Fixed IEDCR scraper, no need to run the the other scraper for now

--- iedcr_scraper.py
+++ iedcr_scraper.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 import timer
 import pandas as pd
 import datetime
+import numpy as np
+import datetime
 ##LETS GET THE PAGE FIRST
 iedcr_url="https://www.iedcr.gov.bd"
 webpg=requests.get(iedcr_url).text
@@ -17,6 +19,53 @@ webpg=requests.get(iedcr_url).text
 bs4soup=BeautifulSoup(webpg, "html.parser")
 #LETS SCRAPE OUT THE DATE FIRST
 dash=bs4soup.find('div', class_='covid-dash-div')
+table=bs4soup.find('table', class_='zebra')
+table_data=[]
+for row in table.findAll('tr'):
+    c=0
+    for c in row.findAll('td'):
+        val1=c.text
+        
+    table_data.append(val1)
+
+totalTests=table_data[2]
+last24hrtests=table_data[1]
+newpos=table_data[3]
+totalpos=table_data[4]
+
+more_counts=dash.findAll('div', class_='counter')
+for i in more_counts:
+    table_data.append(i.text)
+
+newRecs=table_data[5]
+totRecs=table_data[6]
+newDeaths=table_data[7]
+totDeaths=table_data[8]
+
+dateToday=table.find('strong').text
+currDate=''.join([i for i  in dateToday if i in '0123456789-'])
+
+recoveryRate=(int(totRecs)/int(totalpos))*100
+deathRate=(int(totDeaths)/int(totalpos))*100
+
+
+formattedDate=datetime.datetime.strptime(currDate, "%d-%m-%Y").strftime("%d/%m/%Y")
+cols=['Date','TOTAL COVID-19 TESTS', 'LAST 24 Hours Test', 'Covid-19 Positive Cases', 'Last 24Hours Cases', 'Recovered', 'Death Cases', 'Recovery Rate', 'Death Rate', 'New Recoveries', 'Deaths in last 24 hours']
+daily_data=[formattedDate ,totalTests, last24hrtests, totalpos, newpos, totRecs, totDeaths, recoveryRate, deathRate, newRecs, newDeaths]
+df=pd.DataFrame([daily_data],columns=cols)
+
+getPrevData=pd.read_csv('data.csv')
+
+lastUpdated=getPrevData["Date"].iloc[-1]
+toXLformat=datetime.datetime.strptime(lastUpdated, "%d/%m/%Y").strftime("%d-%m-%Y")
+if currDate!=toXLformat:
+    df.to_csv('data.csv', mode='a', header=False, index=False)    
+else:
+    print("Today's data is already in your file")
+
+"""
+
+
 findingdate=dash.findAll('p')
 datetext=findingdate[-3].text
 #GOT THE DATE, NOW TO FILTER OUT THE TEXT
@@ -56,3 +105,5 @@ if currentdate!=toXLformat:
     df.to_csv('data.csv', mode='a', header=False, index=False)    
 else:
     print("Today's data is already in your file")
+    
+"""
