531eff86f66bea0b12a0684ace0e21e32a283044
tusher16 <tusher16@gmail.com>
2020-04-15 12:31:11 +0600

    image and icon updated

--- web-interface/index.php
+++ web-interface/index.php
@@ -181,7 +181,7 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>COVID-19</span></a>
+               <a href="index.php" class="site_title"><img src="images/main_image.png" alt="Covid19BD"  style='height: 100%; width: 100%; object-fit: contain'></a>
             </div>
 
             <div class="clearfix"></div>
@@ -192,7 +192,7 @@
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                  <li><a href="index.php"><i class="fa fa-home"></i> Home <span class="fa"></span></a>
 
                   </li>
                   <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation<span class="fa fa-chevron-down"></span></a>
