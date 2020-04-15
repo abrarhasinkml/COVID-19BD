cb59a84736408d91b9ac1775ba420017765e5b77
mahmud60 <mahmudul.hasan1.cse@ulab.edu.bd>
2020-04-15 21:48:56 +0600

    Fixed minor bugs

--- web-interface/index.php
+++ web-interface/index.php
@@ -53,9 +53,9 @@
 
       $count = count($totalTests);
 
-      $percentagePositive = ($last24HoursCases[$count-1] / $positiveCases[$count-3]) * 100;
+      $percentagePositive = ($last24HoursCases[$count-1] / $positiveCases[$count-2]) * 100;
 
-      $percentageDeath = ($deathsInLast24Hours[$count-1] / $deathCases[$count-3]) * 100;
+      $percentageDeath = ($deathsInLast24Hours[$count-1] / $deathCases[$count-2]) * 100;
 
       $percentageRecovery = $recoveryRate[$count-1] - $recoveryRate[$count-2];
 
