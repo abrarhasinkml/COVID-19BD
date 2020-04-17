3c594e526bfec58d3eb2e46a66a9e0f966767f3f
abrarhasinkml <ahasin12@gmail.com>
2020-04-17 15:21:57 +0600

    Updated regionbased

--- regionBasedData.py
+++ regionBasedData.py
@@ -20,7 +20,7 @@ try:
     soup=BeautifulSoup(request_web, 'html.parser')
     dash=soup.findAll('a')
     
-    theUrlweNeed=dash[-2]['href']
+    theUrlweNeed=dash[-3]['href']
     
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     
